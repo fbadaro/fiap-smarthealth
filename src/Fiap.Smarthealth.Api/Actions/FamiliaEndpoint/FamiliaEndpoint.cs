b7b1eff54b7495a1af7a7cba609d2482ceabe108
Fabio Badaro <fbadaro2@gmail.com>
2022-04-16 15:56:51 -0300

    feat: implementation of familia endpoint, changes in repository

--- src/Fiap.Smarthealth.Api/Actions/FamiliaEndpoint/FamiliaEndpoint.cs
+++ src/Fiap.Smarthealth.Api/Actions/FamiliaEndpoint/FamiliaEndpoint.cs
@@ -6,19 +6,58 @@ public static class FamiliaEndpoint
 {
     public static void MapFamiliaEndpoint(this WebApplication? app)
     {
+        // LIST
         app!.MapGet("/familia", async (IFamiliaService _familiaAppService) =>
             await _familiaAppService.GetAllAsync())
+        .Produces<List<FamiliaDTO>>(StatusCodes.Status200OK)
         .WithName("GetFamilia")
         .WithTags("Familia");
 
-        app!.MapPost("/familia", async (IFamiliaService _familiaAppService, FamiliaDTO familia) =>            
-            await _familiaAppService.CreateAsync(familia))
+        // GET
+        app!.MapGet("/familia/{id}", async (IFamiliaService _familiaAppService, Guid id) =>
+        {
+            var result = await _familiaAppService.GetById(id);
+
+            return result != null
+                ? Results.Ok(result)
+                : Results.NotFound();
+        })
+        .Produces<FamiliaDTO>(StatusCodes.Status200OK)
+        .Produces(StatusCodes.Status404NotFound)
+        .WithName("GetFamiliaById")
+        .WithTags("Familia");
+
+        // POST
+        app!.MapPost("/familia", async (IFamiliaService _familiaAppService, FamiliaDTO familia) =>
+        {
+            var result = await _familiaAppService.CreateAsync(familia);
+
+            return result != null            
+            ? Results.CreatedAtRoute("GetFamiliaById", new { id = result.Id }, familia)
+            : Results.BadRequest("Houve um problema ao salvar o registro");
+        })
         .Produces<FamiliaDTO>(StatusCodes.Status201Created)
         .Produces(StatusCodes.Status400BadRequest)
         .WithName("PostFamilia")
         .WithTags("Familia");
 
-        app!.MapDelete("/fornecedor/{id}", async (IFamiliaService _familiaAppService, Guid id) =>
+        // UPDATE
+        app!.MapPut("/familia/{id}", async (IFamiliaService _familiaAppService, Guid id, FamiliaDTO familia) =>
+        {
+            var result = await _familiaAppService.UpdateAsync(familia);
+
+            return result != null
+                ? Results.NoContent()
+                : Results.BadRequest("Houve um problema ao salvar o registro");
+
+        })
+        .Produces(StatusCodes.Status204NoContent)
+        .Produces(StatusCodes.Status400BadRequest)
+        .WithName("PutFamilia")
+        .WithTags("Familia");
+
+        // DELETE
+        app!.MapDelete("/familia/{id}", async (IFamiliaService _familiaAppService, Guid id) =>
             await _familiaAppService.DeleteAsync(id))
         .Produces(StatusCodes.Status400BadRequest)
         .Produces(StatusCodes.Status204NoContent)
